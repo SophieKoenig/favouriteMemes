f55bc8f731049431e5107fe6217096e7e7c3cb4d
Sophie <81225116+SophieKoenig@users.noreply.github.com>
2022-06-03 09:52:49 +0200

    new meme when clicking button

--- script/script.js
+++ script/script.js
@@ -1,11 +1,27 @@
-function button() {
-  document.getElementById("imgContainer").style.visibility = "hidden";
-}
-button();
+window.onload = visiMeme;
+var myPic = new Array(
+  "images/dogProf.jpg",
+  "images/google.jpg",
+  "images/honestWork.jpg",
+  "images/stackOver.jpg"
+);
+
+// function button() {
+//   document.getElementById("imgContainer").style.visibility = "hidden";
+// }
+// button();
 
 const element = document.getElementById("theBtn");
 element.addEventListener("click", visiMeme);
 
+// function choosePic() {
+//   var randomNum = Math.floor(Math.random() * myPic.length);
+//   document.getElementById("myImg").src = myPic[randomNum];
+// }
+
 function visiMeme() {
-  document.getElementById("imgContainer").style.visibility = "visible";
+  var randomNum = Math.floor(Math.random() * myPic.length);
+  document.getElementById("myImg").src = myPic[randomNum];
+  console.log("functioning");
+  // document.getElementById("imgContainer").style.visibility = "visible";
 }
